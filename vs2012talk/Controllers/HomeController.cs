7108977dcce60b02078ab7ae5fcd23866ef6cebc
JustinJohnWilliams <JustinJohnWilliams@gmail.com>
2012-09-05 16:01:51 -0500

    first commit

---
+++ vs2012talk/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vs2012talk.Models;
+
+namespace vs2012talk.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Modal()
+        {
+            return PartialView("_modal");
+        }
+
+        [HttpPost]
+        public ActionResult Create(IndexViewModel model)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ vs2012talk/Scripts/magic.js
@@ -0,0 +1,13 @@
+﻿var magicNameSpace = {
+
+    doStuff: function (x, y) {
+        /// <param name="x" type="int">magic number</param>
+        /// <param name="y" type="string">magic string</param>
+
+        var a = x + 0;
+        var b = 'your string was: "' + y + '"';
+
+        alert(b + ' and your number was: ' + a);
+    }
+
+}
